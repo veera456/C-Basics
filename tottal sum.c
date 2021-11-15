31b861f4bcb1f37e1190bf417dd9cd590d1c7236
veera456 <93245327+veera456@users.noreply.github.com>
2021-11-15 12:31:44 +0530

    Add files via upload

---
+++ tottal sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	//variable
+	int phy,mat,che,eng,tot;
+	//initiallzation
+	phy=60,mat=50,che=40,eng=50;
+	//calculation
+	tot=phy+mat+che+eng;
+	printf("phy=%d\n,mat=%d\n,che=%d\n,eng=%d\n",phy,mat,che,eng);
+	printf("tot%d",tot);
+}
