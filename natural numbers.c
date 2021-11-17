d663c1b4879ac28a1b2f5a96f31ea531c8e4821b
veera456 <93245327+veera456@users.noreply.github.com>
2021-11-17 12:18:15 +0530

    Add files via upload

---
+++ natural numbers.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+void main ()
+{
+	//variables
+	int a,b;
+	//instalisation
+	a=20;
+	//calculation
+	b=a*(a+1)/2;
+	//outpout
+	printf("sum of first %d natural numbers is %d",a,b);
+}
