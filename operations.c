dd85ccc2a13564eac4759011fbe30abc9bfd09d1
veera456 <93245327+veera456@users.noreply.github.com>
2021-11-17 12:17:51 +0530

    Add files via upload

---
+++ operations.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,s,h,r,m;
+	a=10,b=2;
+	s=a+b,h=a-b,r=a*b,m=a/b;
+	printf("sum of %d and %d is %d\n",a,b,s);
+	printf("difference of %d and %d is %d\n",a,b,h);
+	printf("product of %d and %d is %d\n",a,b,r);
+	printf("quotient of %d and %d is %d\n",a,b,m);
+	
+}
