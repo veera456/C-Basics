9c6afbde96663025e35ee538e91979fab37f8202
veera456 <93245327+veera456@users.noreply.github.com>
2021-12-06 12:21:11 +0530

    Add files via upload

---
+++ divisible by numbers condition.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%4==0 && a%2==0)
+	{
+	     printf("given number is divisibl with 4&2");
+	}
+	else
+	{
+	     printf("given number is not divisible with 4&2");
+	}
+}
